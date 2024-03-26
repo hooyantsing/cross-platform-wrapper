6162bc08f60abef4e14e4e62b0f479059b690174
hooyantsing <hooyantsing@gmail.com>
2024-03-26 19:09:04 +0800

    AbstractLocator.

--- m3u8-client-core/src/main/java/xyz/hooy/m3u8client/nm3u8dlre/AbstractLocator.java
+++ m3u8-client-core/src/main/java/xyz/hooy/m3u8client/nm3u8dlre/AbstractLocator.java
@@ -5,7 +5,6 @@ import xyz.hooy.m3u8client.Locator;
 import xyz.hooy.m3u8client.util.OperatingSystem;
 import xyz.hooy.m3u8client.util.OperatingSystemUtils;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -24,50 +23,21 @@ public abstract class AbstractLocator implements Locator {
     private final String clientVersion;
     private String executableClient;
 
-    protected AbstractLocator(String clientName, String clientVersion) {
+    public AbstractLocator(String clientName, String clientVersion) {
         this.clientName = clientName;
         this.clientVersion = clientVersion;
     }
 
     public void exe() throws IOException {
-        createDirectory(localClientDirectory);
+        createDirectory(Paths.get(localClientDirectory));
         String clientFullName = generateClientName();
-        File sourceFile = new File(jarClientDirector, clientFullName);
-        File targetFile = new File(localClientDirectory, clientFullName);
-        initClient(sourceFile.getAbsolutePath(), targetFile.getAbsolutePath());
-        executableClient = targetFile.getAbsolutePath();
+        Path sourcePath = Paths.get(jarClientDirector, clientFullName);
+        Path targetFile = Paths.get(localClientDirectory, clientFullName);
+        initClient(sourcePath, targetFile);
+        executableClient = targetFile.toString();
     }
 
-    private synchronized void initClient(String sourcePath, String targetPath) throws IOException {
-        if (!Files.isRegularFile(Paths.get(targetPath))) {
-            copyClient(sourcePath, targetPath);
-        }
-        if (OperatingSystemUtils.currentOperatingSystem() != OperatingSystem.Windows) {
-            ProcessBuilder command = new ProcessBuilder().command("/bin/chmod", "755", targetPath);
-            command.start();
-        }
-    }
-
-    private void copyClient(String sourcePath, String targetPath) throws IOException {
-        InputStream is = getClass().getResourceAsStream(sourcePath);
-        if (Objects.isNull(is)) {
-            // Use this for Java 9+ only if required
-            is = ClassLoader.getSystemResourceAsStream(sourcePath);
-            if (Objects.isNull(is)) {
-                // Use this for spring boot with different class loaders
-                ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-                is = classloader.getResourceAsStream(sourcePath);
-            }
-        }
-        if (Objects.nonNull(is)) {
-            Files.copy(Paths.get(sourcePath), Paths.get(targetPath), StandardCopyOption.REPLACE_EXISTING);
-            is.close();
-        } else {
-            throw new IOException("Not found client!");
-        }
-    }
-
-    private String generateClientName() {
+    public String generateClientName() {
         String arch = System.getProperty("os.arch");
         OperatingSystem operatingSystem = OperatingSystemUtils.currentOperatingSystem();
         return standardClientName.replace("#{CLIENT_NAME}", clientName)
@@ -77,10 +47,39 @@ public abstract class AbstractLocator implements Locator {
                 .replace("#{SUFFER}", operatingSystem.getSuffix());
     }
 
-    private void createDirectory(String path) throws IOException {
-        Path directory = Paths.get(path);
-        if (!Files.isDirectory(directory)) {
-            Files.createDirectory(directory);
+    protected synchronized void initClient(Path sourcePath, Path targetPath) throws IOException {
+        if (!Files.isRegularFile(targetPath)) {
+            copyClient(sourcePath, targetPath);
+        }
+        if (OperatingSystemUtils.currentOperatingSystem() != OperatingSystem.Windows) {
+            ProcessBuilder command = new ProcessBuilder().command("/bin/chmod", "755", targetPath.toString());
+            command.start();
+        }
+    }
+
+    protected void copyClient(Path sourcePath, Path targetPath) throws IOException {
+        String source = sourcePath.toString();
+        InputStream is = getClass().getResourceAsStream(source);
+        if (Objects.isNull(is)) {
+            // Use this for Java 9+ only if required
+            is = ClassLoader.getSystemResourceAsStream(source);
+            if (Objects.isNull(is)) {
+                // Use this for spring boot with different class loaders
+                ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+                is = classloader.getResourceAsStream(source);
+            }
+        }
+        if (Objects.nonNull(is)) {
+            Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+            is.close();
+        } else {
+            throw new IOException("Not found client!");
+        }
+    }
+
+    protected void createDirectory(Path path) throws IOException {
+        if (!Files.isDirectory(path)) {
+            Files.createDirectory(path);
         }
     }
 }
